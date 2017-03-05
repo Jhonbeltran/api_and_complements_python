34adba55b4c0870eb927b116443d18f288ed13c9
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-05 15:18:46 -0500

    completo la descarga de la imagen

--- lorempixelAPI.py
+++ lorempixelAPI.py
@@ -3,5 +3,6 @@ from urllib.request import urlopen
 url = "http://lorempixel.com/640/480/sports/"
 placeholder = urlopen(url)
 content = placeholder.read()
-
-print (content)
+file = open("holder.jpg", "wb")
+file.write(content)
+file.close()
