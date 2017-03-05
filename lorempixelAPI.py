3e97141feddc7d472261afa13c2be04fef01bf15
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-05 15:52:17 -0500

    Agrego un for para traer 10 images diferentes con los valores que se definen

--- lorempixelAPI.py
+++ lorempixelAPI.py
@@ -1,8 +1,24 @@
 from urllib.request import urlopen
 
-url = "http://lorempixel.com/640/480/sports/"
-placeholder = urlopen(url)
-content = placeholder.read()
-file = open("holder.jpg", "wb")
-file.write(content)
-file.close()
+url = 'http://lorempixel.com'
+categories = ['abstract', 'animals','business',
+            'cats','city','food', 'night','life',
+            'fashion','people','nature','sports',
+            'technics','transport',]
+
+width = input('Width: ')
+heigth = input('Height: ')
+category = input('Category: ')
+
+
+for img in range(10): 
+    url_req = '%s/%s/%s/%s/%d' % (url,
+                            width,
+                            heigth,
+                            categories[int(category)], img)
+
+    placeholder = urlopen(url_req)
+    content = placeholder.read()
+    file = open('holder_%d.jpg' % img, 'wb')
+    file.write(content)
+    file.close()
