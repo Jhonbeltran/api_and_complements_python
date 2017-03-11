788d3dd19aa07ef667cc05c11c58a1743a1f47f6
Jhonbeltran <jbeltranleon@gmail.com>
2017-03-11 17:49:52 -0500

    Agrego lector de json

---
+++ reading_json.py
@@ -0,0 +1,11 @@
+#Este codigo lo puedo poner en la view que quiero que use el json 
+
+import json
+from urllib.request import urlopen
+
+url = 'http://localhost:8000/api/v1/answers/?format=json'
+placeholder = urlopen(url)
+
+data = json.loads(placeholder.read())
+
+print(data)
